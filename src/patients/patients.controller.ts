1a61edd1d116417dc0c5870b8d43930647d2adf5
ahmd1c <gwerqweldiv@gmail.com>
2024-09-13 20:58:23 +0300

    added User decorator and used it in patient ctrl

--- src/patients/patients.controller.ts
+++ src/patients/patients.controller.ts
@@ -6,21 +6,21 @@ import {
   Patch,
   Param,
   Delete,
-  Request,
 } from '@nestjs/common';
 import { PatientsService } from './patients.service';
 import { CreatePatientDto } from './dto/create-patient.dto';
 import { UpdatePatientDto } from './dto/update-patient.dto';
+import User from 'src/utils/decorators/USER.decorator';
 
 @Controller('patients')
 export class PatientsController {
   constructor(private readonly patientsService: PatientsService) {}
 
   @Post()
-  create(@Body() createPatientDto: CreatePatientDto, @Request() req) {
+  create(@Body() createPatientDto: CreatePatientDto, @User() user) {
     return this.patientsService.create({
       ...createPatientDto,
-      id: req.user?.id,
+      id: user?.id,
     });
   }
 
